3da57cdaebd73f64ba582b26ae8ad1ccf4eca386
Mehrnaz Pishyar <mpishyar.de@gmail.com>
2024-11-19 11:43:57 +0100

    create project pages and set Routes for them & set condition for create account button in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import Navbar from "./components/Navbar"
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import MyProfile from "./pages/MyProfile";
+import Appointment from "./pages/Appointment";
+import MyAppointments from "./pages/MyAppointments";
 
 function App() {
-
-
   return (
     <div className="mx-4 sm:mx-[10%]">
-    <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/appointment/:docId" element={<Appointment />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
